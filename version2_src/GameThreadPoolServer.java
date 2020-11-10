d95ddfdc54eb539492618300cb2320a10169dbb4
Victor-Kumi <victorkumituah@gmail.com>
2020-11-10 14:22:53 +0000

    Added version 2 of game app. Version doesn't allow client to click unduly click on GUI

---
+++ version2_src/GameThreadPoolServer.java
@@ -0,0 +1,92 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/**
+ * Server class accepts several connections and pairs clients for a game. One thread is made per game
+ * and there are 8 threadpools services. So server can support 8 games at a go.
+ * @author Victor Tuah Kumi
+ *
+ */
+public class GameThreadPoolServer implements Constants{
+
+	private Socket aSocket;
+	private ServerSocket serverSocket;
+	private PrintWriter socketOut;
+	private BufferedReader socketIn;
+	private int numOfConnections;
+	private Player xPlayer, oPlayer;
+	private String xName, oName;
+	private ExecutorService pool;
+    //private Board board;
+	public GameThreadPoolServer() {
+		try {
+			serverSocket = new ServerSocket(9898);
+			pool = Executors.newFixedThreadPool(2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//board = new Board();
+	}
+
+	public void runServer() {
+        System.out.println("Server is running..."); 
+		try {
+			while (true) {
+				aSocket = serverSocket.accept();
+				System.out.println("Connection " + (numOfConnections + 1) + " accepted by the server!");
+				socketIn = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+				socketOut = new PrintWriter(aSocket.getOutputStream(), true);
+				
+				numOfConnections++;
+				
+				if (numOfConnections % 2 == 1) {
+					//board.setSocket(socketOut);
+					//board.display();
+					socketOut.println("Please enter your name");
+					xName = socketIn.readLine();
+					xPlayer = new Player(xName, LETTER_X);
+				    xPlayer.setSocket(socketIn, socketOut);
+					socketOut.println("Waiting for your opponent to connect");
+				}
+				else if(numOfConnections % 2 == 0) {
+					//board.setSocket(socketOut);
+					//board.display();
+					socketOut.println("Your opponent is " + xName + ", please enter your name");
+					oName = socketIn.readLine();
+				    oPlayer = new Player(oName, LETTER_O);
+				    xPlayer.getSocketOut().println("Connected opponent " + oName);
+				    oPlayer.setSocket(socketIn, socketOut);
+				    socketOut.println("Please wait for " + xName + " to start game");
+				    Game theGame = new Game(xPlayer, oPlayer);
+				    
+					pool.execute(theGame);
+					System.out.println("New game instance started in one threadpool");
+				}
+			
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		pool.shutdown();
+		
+		try {
+			socketIn.close();
+			socketOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		GameThreadPoolServer myServer = new GameThreadPoolServer();
+		myServer.runServer();
+	}
+}
---
+++ version2_src/Player.java
@@ -0,0 +1,106 @@
+import java.io.*;
+/**
+ * Player class is responsible for making the play and checking for possible win or tie.
+ * It also is responsible for relaying every move made to the front end; the other opponent;
+ * @author Victor Tuah Kumi
+ *
+ */
+public class Player {
+    private String name;
+    private Board board;
+    private Player opponent;
+    private char mark;
+    private BufferedReader socketIn;
+    private PrintWriter socketOut;
+    public Player(String name, char mark){
+        this.name = name;
+        this.mark = mark;
+    }
+    
+    /**
+     * While there is no winner, make move, check for possible winner and exit game, if not make opponent play.
+     */
+    public void play() {
+        while (!isWinner()){
+            this.makeMove();
+            if(!isWinner())
+            	opponent.play();
+        }
+    }
+    
+    /**
+     * Make move and update opponent on move made
+     */
+    public void makeMove(){
+        int row;
+        int col;
+        socketOut.println(this.name + ", click a box to play");
+        try {
+        	String rowColStr = socketIn.readLine();
+			String[] rowCol = rowColStr.split(" ");
+			row = Integer.parseInt(rowCol[0]);
+			col = Integer.parseInt(rowCol[1]);
+	        if (board.getMark(row, col) == board.SPACE_CHAR) {
+	        	opponent.socketOut.println(rowColStr);
+	            board.addMark(row, col, mark);
+	        } 
+	        else
+	            makeMove();
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}   
+    }
+    
+    public void setOpponent(Player oPlayer) {
+        this.opponent = oPlayer;
+    }
+
+    public void setBoard(Board theBoard) {
+        this.board = theBoard;
+    }
+    
+    /**
+     * check for win, tie or none
+     * @return false if no winner, otherwise game ends
+     */
+    public boolean isWinner(){
+        boolean winner = false;
+        boolean tie = board.isFull();
+        boolean xWins = board.xWins();
+        boolean oWins = board.oWins();
+        if (tie || xWins || oWins){
+            winner = true;
+            if (tie) {
+            	socketOut.println("GAME OVER!!!.. A TIE GAME");
+            	opponent.socketOut.println("GAME OVER!!!.. A TIE GAME");
+            }   
+            else {
+            	socketOut.println("GAME OVER!!! " + this.name + " wins");
+            	opponent.socketOut.println("GAME OVER!!! " + this.name + " wins");
+            }
+            socketOut.println("GAME OVER");
+            opponent.socketOut.println("GAME OVER");
+        }
+        return winner;
+    }
+    
+    public void setSocket(BufferedReader socketIn, PrintWriter socketOut) {
+    	this.socketIn = socketIn;
+    	this.socketOut = socketOut;
+    }
+    public PrintWriter getSocketOut() {
+    	return this.socketOut;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+    //Method unused
+    public void printBoardToBothSocketOuts() {
+    	board.setSocket(socketOut);
+    	board.display();
+
+    	board.setSocket(opponent.getSocketOut());
+    	board.display();
+    }
+}
